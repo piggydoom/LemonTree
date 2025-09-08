3b579ea44edfae14487b9f4890e043bc4ab75e47
artist piggy <artistpiggy@gmail.com>
2025-09-08 14:12:04 +1000

    finished order menu, added about text

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const submitButton = document.getElementById('submitButton')
+// let submitButton = document.getElementById('submitButton')
 
-function submitPressed(){
-console.log("worked");
+
+
+function saveAllData(){
+    
+let firstName= document.getElementById('firstName')
+let middleName= document.getElementById('middleName')
+let lastName= document.getElementById('lastName')
+let phoneNumber= document.getElementById('phoneNum')
+let location= document.getElementById('location')
+let description= document.getElementById('desc')
+let start= document.getElementById('start')
+let end= document.getElementById('end')
+
+console.log(firstName.value)
+console.log(middleName.value)
+console.log(lastName.value)
+console.log(phoneNumber.value)
+console.log(location.value)
+console.log(description.value)
+console.log(start.value, "-", end.value)
+
 };
 
-document.getElementById('submitButton');{
-    addEventListener(onclick)
-console.log("delta");
-};
